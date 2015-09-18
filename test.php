ce0dcbe9a804e0847af06eeba0411f710fca57fd
gwilford <gwilford+git@gmail.com>
2015-09-18 20:29:38 +0100

    cleanup redirect page

--- test.php
+++ test.php
@@ -5,22 +5,25 @@ $m = new PHPMoves\Moves(Config::$client_id,Config::$client_secret,Config::$redir
 
 if (isset($_GET['code'])) {
     $request_token = $_GET['code'];
-
     $tokens = $m->auth($request_token);
-    var_dump($tokens);
 
     // retrieve and update the users DB
-    $str = file_get_contents($storedir . "/users.json");
+    $str = file_get_contents(Config::$storedir . "/users.json");
     if ($str) {
 	    $users = json_decode($str, true);
     } else {
 	    $users = array();
     }
-    $users[$tokens['userid']] = $tokens;
-    file_put_contents($storedir . "/users.json", json_encode($users));
-
-    //Save this token for all future request for this user
-    $access_token = $tokens['access_token'];
-    echo json_encode($m->get_profile($access_token));
+    $users[$tokens['user_id']] = $tokens;
+    file_put_contents(Config::$storedir . "/users.json", json_encode($users, JSON_PRETTY_PRINT));
 }
 ?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8"/>
+    </head>
+    <body>
+        Thank you - you are now registered with Moves-driven
+    </body>
+</html>
