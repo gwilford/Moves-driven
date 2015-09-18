b22f5198693869d372e9ff07f7418c6bab9ee366
gwilford <gwilford+git@gmail.com>
2015-09-18 16:38:22 +0100

    change auth() to return whole array

--- Moves.php
+++ Moves.php
@@ -73,7 +73,8 @@
                         $result = curl_exec($ch);
                         curl_close($ch);
                         $token = json_decode($result, True);
-                        return array('access_token' => $token['access_token'], 'refresh_token' => $token['refresh_token']);
+                        //return array('access_token' => $token['access_token'], 'refresh_token' => $token['refresh_token']);
+			return($token);
                 }
                         
 
