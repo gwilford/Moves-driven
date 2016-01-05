a841c6733aaff4e144bced676c203ccd808e5a43
gwilford <gwilford+git@gmail.com>
2016-01-05 09:14:40 +0000

    General cleanup

--- extract-all.php
+++ extract-all.php
@@ -3,16 +3,19 @@ include_once 'Moves.php';
 include_once 'config.php';
 $m = new PHPMoves\Moves(Config::$client_id,Config::$client_secret,Config::$redirect_url);
 
-// retrieve and update the users DB
-$str = file_get_contents(Config::$storedir . "/users.json");
+// retrieve the users DB
+$ufile = Config::$storedir . "/users.json";
+$users = json_decode(file_get_contents($ufile), TRUE);
+
+// initialise date parsing
 $today = new DateTime();
 echo $today->format('Ymd') . "\n";
 $interval = DateInterval::createfromdatestring('+1 day');
 
-$users = json_decode($str, TRUE);
+// cycle through each user
 foreach($users as $key => $user) {
 	echo $user['user_id'] . "\n";
-	// validate user token
+	// validate existing user token
 	if (!$m->validate_token($user['access_token'])) {
 		$refresh = $m->refresh($user['access_token']);
 		echo json_encode($refresh, JSON_PRETTY_PRINT);
@@ -26,13 +29,13 @@ foreach($users as $key => $user) {
 			$user = array_merge($user, $refresh);
 			$users[$key] = $user;
 		}
-		file_put_contents(Config::$storedir . "/users.json", json_encode($users, JSON_PRETTY_PRINT));
+		file_put_contents($ufile, json_encode($users, JSON_PRETTY_PRINT));
 	}
 	
 	// read in existing extract file
-	$file = Config::$storedir . "/extract" . $key . ".json";
-	if (is_file($file)) {
-		$moves = json_decode(file_get_contents($file), TRUE);
+	$efile = Config::$storedir . "/extract" . $key . ".json";
+	if (is_file($efile)) {
+		$moves = json_decode(file_get_contents($efile), TRUE);
 	} else {
 		$moves = array();
 	}
@@ -40,13 +43,15 @@ foreach($users as $key => $user) {
 	//exit;
 	//echo array_keys($moves);
 	$date = new DateTime($user['profile']['firstDate']);
-	// cycle through all *new* dates 1 days at a time
+	// cycle through all dates 1 days at a time
 	while ($date < $today) {
 		$day = $date->format('Ymd');
 		if (!isset($moves[$day]) || $moves[$day] == "null") {
+			// a new date...
 			echo $day . ", ";
 			$ret = $m->get_range($user['access_token'],
                           		'/user/storyline/daily', $day, $day); 
+                        // ratelimit
 			sleep(1);
 			if ($ret && $ret != NULL) {
 				$moves[$day] = $ret;
@@ -60,9 +65,6 @@ foreach($users as $key => $user) {
 		}
 	}
 	echo "\n";
-	file_put_contents(Config::$storedir . "/extract" . $key . ".json", 
-		//json_encode($moves, JSON_PRETTY_PRINT));
-		json_encode($moves));
-			    
+	file_put_contents($efile, json_encode($moves));
 }
 ?>
